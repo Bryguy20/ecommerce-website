fda468cf58b283cbaee2d5f81bcfe166f33e8c1d
Fizzy <happypanda117@gmail.com>
2022-08-27 12:59:15 -0400

    added images to db

--- seeds/itemData.js
+++ seeds/itemData.js
@@ -2,159 +2,160 @@ const { Product } = require('../models');
 
 const productdata = [
   {
-    "name": "Awesome Generic Coffee Pods",
+    "item": "Awesome Generic Coffee Pods",
     "description": "Ten count of medium roast amazing arabica coffee pods.",
     "price": 10,
-    "category_id": 1
+    "filename":"coffeePod.png"
   },
   {
-    "name": "Sun Glasses",
+    "item": "Sun Glasses",
     "description": "Desinger style fashionable polarized Glasses.",
     "price": 50,
-    "category_id": 5
+    "filename":"sunglasses.png"
   },
   {
-    "name": "Phone mount",
+    "item": "Phone mount",
     "description": "Easy to use phone mount for your car that fits into any cup holder",
     "price": 20,
-    "category_id": 2
+    "filename":"PhoneMount"
   },
   {
-    "name": "Mouse pad",
+    "item": "Mouse pad",
     "description": "Dark mouse pad with foam rubber backing to prevent slipping.",
     "price": 5,
-    "category_id": 2
+    "filename":"MousePad.png"
   },
   {
-    "name": "Small Mirror",
+    "item": "Small Mirror",
     "description": "small mirror that is easily adjustable",
     "price": 10,
-    "category_id": 1
+    "filename":"SmallMirror.png"
   },
   {
-    "name": "Fuzzy Bathroom Rug",
+    "item": "Fuzzy Bathroom Rug",
     "description": "Ultra Plush comfortable bathroom mat ",
     "price": 25,
-    "category_id": 1
+    "filename":"Rug.png"
   },
   {
-    "name": "organization tray",
+    "item": "organization tray",
     "description": "This stylish drawer is a space-saving design that requires no tools and includes nine customizable compartments.",
     "price": 10,
-    "category_id": 1
+    "filename":"OrganizationTrar.png"
   },
   {
-    "name": "All purpose cleaner",
+    "item": "All purpose cleaner",
     "description": "This all in one cleaner can clean anything in just one spray",
     "price": 10,
-    "category_id": 1
+    "filename":"Cleaner.png"
   },
   {
     "item": "RGB gaming mousse",
     "description": "the lights will improve your gameplay",
     "price": 30,
-    "category_id": 2
+    "filename":"mouse.png"
   },
   {
-    "name": "usb type c charger",
+    "item": "usb type c charger",
     "description": "three foot charging cable and brick",
     "price": 10,
-    "category_id": 2
+    "filename":"usb.png"
   },
   {
-    "name": "mechanical keyboard",
+    "item": "mechanical keyboard",
     "description": "distinctive and fun clicky sounds with each keystroke",
     "price": 40,
-    "category_id": 2
+    "filename":"Keyboard.png"
   },
   {
-    "name": "Hour glass",
+    "item": "Hour glass",
     "description": "Time flys by while having fun! wach these grains of sand fall with style",
     "price": 10,
-    "category_id": 2
+    "filename":"Hourglass.png"
   },
   {
-    "name": "Classic art Painting",
+    "item": "Classic art Painting",
     "description": "the classic great wave painting",
     "price": 40,
-    "category_id": 3
+    "filename":"GreatWave.png"
   },
   {
-    "name": "small sculpture",
+    "item": "small sculpture",
     "description": "this decorative little statue will impress and add culture to your living space",
     "price": 40,
-    "category_id": 3
+    "filename":"Sculpture.png"
   },
   {
     "item": "Pokemon card set",
     "description": "These things are still collectables rite? who know you might just get that holographic charizard card",
-    "price": 10
+    "price": 10,
+    "filename":"Pokemon.png"
   },
   {
-    "item": "smat TV",
+    "item": "smart TV",
     "description": "42 inch 4k TV built in wifi support for all the streaming services",
     "price": 200,
-    "category_id": 2
+    "filename":"SmartTv.png"
   },
   {
-    "name": "PS5",
+    "item": "PS5",
     "description": "the scalpers are no longer hoarding all the gaming consoles get yours now!",
     "price": 499,
-    "category_id": 2
+    "filename":"ps5.png"
   },
   {
-    "name": "monopoly",
+    "item": "monopoly",
     "description": "this classic board game is fun for the whole family",
     "price": 10,
-    "category_id": 4
+    "filename":"monopoly.png"
   },
   {
-    "name": "Green Polo shirt",
+    "item": "Green Polo shirt",
     "description": "classic style to match your amazing look",
     "price": 40,
-    "category_id": 5
+    "filename":"polo.png"
   },
   {
-    "name": "Blue jeans",
+    "item": "Blue jeans",
     "description": "every closet needs a pair of classic blue jeans",
     "price": 40,
-    "category_id": 5
+    "filename":"jeans.png"
   },
   {
-    "name": "running shoes",
+    "item": "running shoes",
     "description": "these shoes will make your weekly jog much more comforatable",
     "price": 50,
-    "category_id": 5
+    "filename":"shoes.png"
   },
   {
-    "name": "cute black dress",
+    "item": "cute black dress",
     "description": "the perfect dress for any occasion",
     "price": 40,
-    "category_id": 5
+    "filename":"BlackDress.png"
   },
   {
-    "name": "power drill",
+    "item": "power drill",
     "description": "this power drill will have you completing all those DIY projects in no time",
     "price": 40,
-    "category_id": 6
+    "filename":"Drill.png"
   },
   {
-    "name": "adjustable wrench",
+    "item": "adjustable wrench",
     "description": "a wrench similar to an open end wrench but having one fixed jaw and one adjustable jaw.",
     "price": 10,
-    "category_id": 6
+    "filename":"Wrench.png"
   },
   {
-    "name": "Hammer",
+    "item": "Hammer",
     "description": "now everything looks like a nail",
     "price": 10,
-    "category_id": 6
+    "filename":"Hammer.png"
   },
   {
-    "name": "screw driver",
+    "item": "screw driver",
     "description": "a tool that is used for turning screws",
     "price": 10,
-    "category_id": 6
+    "filename":"ScrewDriver.png"
   }
 ]
 
