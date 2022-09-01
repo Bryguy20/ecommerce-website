07794381b937f3b3ef7cc0363d2e6f914d46e433
basidialsnail <dmmerchant@gmail.com>
2022-08-31 20:42:05 -0400

    Working on + - buttons. Code does not work

--- controllers/api/cart.js
+++ controllers/api/cart.js
@@ -10,15 +10,28 @@ router.post('/:part_id', async (req, res) => {
     if (!req.session.loggedIn) {
         res.redirect('/login');
     } else {
+        console.log(req.session.userCart)
         try {
-            let dbCartData = await CartProduct.create({ cart_id: req.session.userCart, product_id: req.params.part_id });
-            dbCartData = dbCartData.get({ plain: true });
-
+            let dbCartData = await CartProduct.findOne({
+                where: {
+                    cart_id: req.session.userCart,
+                    product_id: req.params.part_id,
+                }
+            });
+            
+            console.log(dbCartData);
             if (!dbCartData) {
-                dbCartData = await CartProduct.create({ cart_id: req.session.userCart, product_id: req.params.part_id });
+                console.log('What?')
+                dbCartData = await CartProduct.create({ 
+                    cart_id: req.session.userCart, 
+                    product_id: req.params.part_id,
+                    qty: 1 });
             } else {
+                console.log('Why?')
+                dbCartData = dbCartData.get({ plain: true });
+                quantity = dbCartData.qty ++;
                 dbCartData = await CartProduct.update(
-                    { qty: dbCartData.qty++ },
+                    { qty: quantity },
                     {
                         where: {
                             cart_id: req,
@@ -26,7 +39,7 @@ router.post('/:part_id', async (req, res) => {
                         }
                     })
             }
-
+            dbCartData = dbCartData.get({ plain: true });
             res.status(200).json(dbCartData);
         } catch (err) {
             res.status(400).json(err);
@@ -47,23 +60,26 @@ router.delete('/:part_id', async (req, res) => {
                 }
             });
 
-            dbCartData = dbCartData.get({ plain: true });
-
+            
+            if(dbCartData){
+                dbCartData = dbCartData.get({ plain: true });
+            };
 
             if (dbCartData.qty > 1) {
+                var quantity = dbCartData.qty --
                 dbCartData = await CartProduct.update(
                     { qty: cartData.qty-- },
                     {
                         where: {
-                            cart_id: req,
+                            cart_id: req.session.userCart,
                             product_id: req.params.part_id
                         }
                     })
                 cartData = await CartProduct.create({ cart_id: req.session.userCart, product_id: req.params.part_id });
-            } else if (dbCartData == 1) {
+            } else if (dbCartData.qty == 1) {
                 dbCartData = await CartProduct.destroy({
                     where: {
-                        cart_id: req,
+                        cart_id: req.session.userCart,
                         product_id: req.params.part_id
                     }
                 });
